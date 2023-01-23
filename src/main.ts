e62dda338937d834fdf2f0d2ecc5054227f6dd42
Nail Badiullin <badiullin.nail@gmail.com>
2023-01-23 21:22:47 +0400

    main add regexp

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     credentials: true,
-    origin: '*',
+    origin: /(.*)/,
   });
   await app.listen(8080);
 }
