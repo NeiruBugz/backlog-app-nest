2ad7d9fc3f4d3582b1f83ebd758c8052f528e0ba
Nail Badiullin <badiullin.nail@gmail.com>
2023-01-23 21:21:13 +0400

    main update cors options

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    credentials: true,
+    origin: '*',
+  });
   await app.listen(8080);
 }
 bootstrap();
