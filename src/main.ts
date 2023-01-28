4a68c85d39fbcb48e52075a44d62b78f9582c58f
Nail Badiullin <badiullin.nail@gmail.com>
2023-01-28 17:31:33 +0400

    feature/search debug cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:3000' });
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://localhost:3000', '*'],
+    credentials: true,
+    methods: ['GET', 'POST'],
+  });
   await app.listen(8080);
 }
 bootstrap();
