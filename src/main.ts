20293ee4256f215507d12ffe66c3bab89e239aac
Nail Badiullin <badiullin.nail@gmail.com>
2023-01-28 21:47:36 +0400

    feature/search update cors origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: /(http|https)\:\/\/((localhost:3000$)|(backlog\-.*(\.|\-)app))/,
+    origin: [
+      'http://localhost:3000',
+      'https://localhost:3000',
+      'https://backlog-app.vercel.app',
+    ],
     credentials: true,
     methods: ['GET', 'POST'],
   });
